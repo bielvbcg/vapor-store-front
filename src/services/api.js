44249c011fed5de1ac6acb3a02a03473c1e31db6
Kl3yton <kleyton_vccamargo@hotmail.com>
2022-02-13 22:12:21 -0300

    fix: fix url

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL ='https://vapor-store-back.herokuapp.com/'
+const BASE_URL ='https://vapor-store-back.herokuapp.com'
 
 async function postCheckout (obj, token){
     const promise = await axios.post(BASE_URL+'/checkout', obj, {headers: {Authorization: `Bearer ${token}`}})
